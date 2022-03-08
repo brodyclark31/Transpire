674b121c6139106a634d6e8de989c0339d6c231a
brodyclark <brodyclark@mymail.mines.edu>
2022-03-07 20:12:15 -0700

    bidning login page

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/LoginPageFragment.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/LoginPageFragment.kt
@@ -5,18 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.brodyclark_l2.transpire.databinding.LoginPageBinding
 
 
 class LoginPageFragment: Fragment() {
 
-//    private var _binding: Fragment? = null
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = Fragment.inflate(inflater, container, false)
-//        return binding.root
-//    }
+    private var _binding: LoginPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LoginPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
