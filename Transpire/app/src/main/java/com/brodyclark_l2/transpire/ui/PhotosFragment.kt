3aff8c236704a74fc87de30c94770859712d6461
Jarod Clark <jarodclark@mymail.mines.edu>
2022-03-15 17:16:38 -0600

    Alpha Release completed ?

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/PhotosFragment.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/PhotosFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.brodyclark_l2.transpire.databinding.PhotosPageBinding
 
@@ -18,6 +19,14 @@ class PhotosFragment: Fragment() {
     ): View {
         _binding = PhotosPageBinding.inflate(inflater, container, false)
 
+        val postPhotoButton = binding.postPhotoButton
+        postPhotoButton.setOnClickListener {
+            Toast.makeText(requireContext(), "This will access camera or camera roll", Toast.LENGTH_SHORT).show()
+        }
+        val postCommentButton = binding.postCommentButton
+        postCommentButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Popup window for comment to be posted", Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 }
