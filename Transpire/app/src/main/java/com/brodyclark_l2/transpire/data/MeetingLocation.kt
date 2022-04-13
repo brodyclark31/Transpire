0e4a6d3105ce872d28b3cf235b6bb4794f834d03
Jarod Clark <jarodclark@mymail.mines.edu>
2022-04-12 19:25:13 -0600

    refactoring location

---
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/data/MeetingLocation.kt
@@ -0,0 +1,14 @@
+
+package com.brodyclark_l2.transpire.data
+
+import java.io.Serializable
+import java.util.*
+
+data class MeetingLocation(val name:String,
+                           val addressLink:String,
+                           val lowAge:Int,
+                           val highAge:Int,
+                           val rating: Double,
+                           var id: UUID = UUID.randomUUID()): Serializable {
+
+                    }
--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/HomePageFragment.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/HomePageFragment.kt
@@ -6,12 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.brodyclark_l2.transpire.data.Location
-import kotlin.random.Random.Default.nextInt
+import com.brodyclark_l2.transpire.data.MeetingLocation
 import com.brodyclark_l2.transpire.databinding.HomePageBinding
-import java.util.*
-import kotlin.math.min
-import kotlin.random.Random.Default.nextInt
 
 class HomePageFragment: Fragment() {
 
@@ -61,14 +57,14 @@ class HomePageFragment: Fragment() {
         return binding.root
     }
 
-    val rockRestLodge = Location("Rock Rest Lodge", "16005 Mt Vernon Rd, Golden, CO 80401", 21, 50,4.3)
-    val aceHigh = Location("Ace-Hi Tavern", "1216 Washington Ave, Golden, CO 80401", 35, 80, 4.3)
-    val barrelsBottlesBrewery = Location("Barrels & Bottles Brewery", "600 12th St #160, Golden, CO 80401", 25, 80, 4.7)
-    val theUnderground = Location("The Underground", "1224 Washington Ave, Golden, CO 80401", 21, 45, 4.3)
-    val minersSaloon = Location("Miners Saloon", "1109 Miner's Alley, Golden, CO 80401", 23, 70, 4.5)
-    val bars = listOf<Location>(rockRestLodge, aceHigh, barrelsBottlesBrewery, theUnderground, minersSaloon)
+    val rockRestLodge = MeetingLocation("Rock Rest Lodge", "16005 Mt Vernon Rd, Golden, CO 80401", 21, 50,4.3)
+    val aceHigh = MeetingLocation("Ace-Hi Tavern", "1216 Washington Ave, Golden, CO 80401", 35, 80, 4.3)
+    val barrelsBottlesBrewery = MeetingLocation("Barrels & Bottles Brewery", "600 12th St #160, Golden, CO 80401", 25, 80, 4.7)
+    val theUnderground = MeetingLocation("The Underground", "1224 Washington Ave, Golden, CO 80401", 21, 45, 4.3)
+    val minersSaloon = MeetingLocation("Miners Saloon", "1109 Miner's Alley, Golden, CO 80401", 23, 70, 4.5)
+    val bars = listOf<MeetingLocation>(rockRestLodge, aceHigh, barrelsBottlesBrewery, theUnderground, minersSaloon)
 
-    fun returnBar(bars: List<Location>, age: Int): Location{
+    fun returnBar(bars: List<MeetingLocation>, age: Int): MeetingLocation{
         while(true){
             var index: Int = (0..5).random()
             if(bars[index].lowAge <= age <= bars[index].highAge)
