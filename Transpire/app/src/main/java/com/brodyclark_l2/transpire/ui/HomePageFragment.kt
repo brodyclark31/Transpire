6d3178826766cb089418faaa9c43daa46123daa2
Levi Johnson <ljohnson1@mymail.mines.edu>
2022-04-12 19:14:07 -0600

    trying to get bullshit working

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/HomePageFragment.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/HomePageFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.brodyclark_l2.transpire.data.location
+import com.brodyclark_l2.transpire.data.Location
+import kotlin.random.Random.Default.nextInt
 import com.brodyclark_l2.transpire.databinding.HomePageBinding
+import java.util.*
+import kotlin.math.min
+import kotlin.random.Random.Default.nextInt
 
 class HomePageFragment: Fragment() {
 
@@ -57,11 +61,15 @@ class HomePageFragment: Fragment() {
         return binding.root
     }
 
-    val rockRestLodge = location("Rock Rest Lodge", "16005 Mt Vernon Rd, Golden, CO 80401", 21, 50,4.3)
-    val aceHigh = location("Ace-Hi Tavern", "1216 Washington Ave, Golden, CO 80401", 35, 80, 4.3)
-    val barrelsBottlesBrewery = location("Barrels & Bottles Brewery", "600 12th St #160, Golden, CO 80401", 25, 80, 4.7)
-   // val theUnderground = location
+    val rockRestLodge = Location("Rock Rest Lodge", "16005 Mt Vernon Rd, Golden, CO 80401", 21, 50,4.3)
+    val aceHigh = Location("Ace-Hi Tavern", "1216 Washington Ave, Golden, CO 80401", 35, 80, 4.3)
+    val barrelsBottlesBrewery = Location("Barrels & Bottles Brewery", "600 12th St #160, Golden, CO 80401", 25, 80, 4.7)
+    val theUnderground = Location("The Underground", "1224 Washington Ave, Golden, CO 80401", 21, 45, 4.3)
+    val minersSaloon = Location("Miners Saloon", "1109 Miner's Alley, Golden, CO 80401", 23, 70, 4.5)
+    val bars = listOf<Location>(rockRestLodge, aceHigh, barrelsBottlesBrewery, theUnderground, minersSaloon)
 
-
-    val bars = listOf<location>()
+    fun returnBar(bars: List<Location>, age: Int){
+        var index: Int = Random.nextInt(from = 0, until = 5)
+        Random.nextInt
+    }
 }
