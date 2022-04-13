157d6f5668e1c312f794088641464c52e7fde537
Levi Johnson <ljohnson1@mymail.mines.edu>
2022-04-12 18:12:22 -0600

    added the location data class

---
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/location.kt
@@ -0,0 +1,4 @@
+package com.brodyclark_l2.transpire.ui
+
+
+data class location(val name:String, val addressLink:String, val lowAge:int, val highAge:int, val rating: Int, var id: UUID = UUID.randomUUID()): Serializable
