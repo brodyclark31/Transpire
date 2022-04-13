559fc792210619a398ba89f00d8493e9ac38d2bc
Levi Johnson <ljohnson1@mymail.mines.edu>
2022-04-12 18:17:33 -0600

    correct loction for location file

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/location.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/location.kt
@@ -1,4 +1,4 @@
-package com.brodyclark_l2.transpire.ui
 
+package com.brodyclark_l2.transpire
 
 data class location(val name:String, val addressLink:String, val lowAge:int, val highAge:int, val rating: Int, var id: UUID = UUID.randomUUID()): Serializable
