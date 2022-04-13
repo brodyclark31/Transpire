7221c2d6dc7e89b07689fc3e15a28f54f2f69070
Jarod Clark <jarodclark@mymail.mines.edu>
2022-04-12 18:48:01 -0600

    refactoring

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/data/database/TranspireDatabase.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/data/database/TranspireDatabase.kt
@@ -1,2 +1,13 @@
 package com.brodyclark_l2.transpire.data.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.brodyclark_l2.transpire.data.UserData
+import com.brodyclark_l2.transpire.data.Location
+
+@Database(entities = [UserData::class, Location::class], version = 1)
+@TypeConverters(UserData::class, Location::class)
+abstract class TranspireDatabase : RoomDatabase() {
+
+}
