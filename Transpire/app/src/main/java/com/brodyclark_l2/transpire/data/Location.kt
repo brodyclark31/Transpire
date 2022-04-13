9c1b25dc8b8baa4c8bfcf3511fbb36a66be90ad2
Levi Johnson <ljohnson1@mymail.mines.edu>
2022-04-12 18:25:00 -0600

    working now

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/data/Location.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/data/Location.kt
@@ -1,4 +1,7 @@
 
 package com.brodyclark_l2.transpire.data
 
+import java.io.Serializable
+import java.util.*
+
 data class location(val name:String, val addressLink:String, val lowAge:Int, val highAge:Int, val rating: Int, var id: UUID = UUID.randomUUID()): Serializable
