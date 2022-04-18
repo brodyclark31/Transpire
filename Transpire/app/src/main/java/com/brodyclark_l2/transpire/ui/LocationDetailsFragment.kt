bae38ebbd3db21b255e32417a05ed5cb6674a0f1
Levi Johnson <ljohnson1@mymail.mines.edu>
2022-04-17 20:42:04 -0600

    updated the location tab

--- Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/LocationDetailsFragment.kt
+++ Transpire/app/src/main/java/com/brodyclark_l2/transpire/ui/LocationDetailsFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.brodyclark_l2.transpire.databinding.LocationDetailsBinding
+import com.brodyclark_l2.transpire.viewmodels.UserDataViewModel
+import com.brodyclark_l2.transpire.viewmodels.ViewModelFactory
 
 class LocationDetailsFragment: Fragment() {
+
+    private lateinit var userDataViewModel: UserDataViewModel
 
     private var _binding: LocationDetailsBinding? = null
     private val binding get() = _binding!!
@@ -18,6 +23,16 @@ class LocationDetailsFragment: Fragment() {
     ): View {
         _binding = LocationDetailsBinding.inflate(inflater, container, false)
 
+        val factory =  ViewModelFactory(requireContext())
+        userDataViewModel = ViewModelProvider(this, factory).get(UserDataViewModel::class.java)
+        setValues()
         return binding.root
+    }
+
+    private fun setValues(){
+        binding.location.text = userDataViewModel.returnMeetingLocation.name
+        binding.address.text = userDataViewModel.returnMeetingLocation.addressLink
+        binding.yelpReview.text = userDataViewModel.returnMeetingLocation.rating.toString()
+
     }
 }
